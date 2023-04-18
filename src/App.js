291fa7b7d942d405e0262037e6ab42fc8ad5647e
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-04-18 18:01:21 +0900

    입력상자 제어

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { useState} from "react";
+import Control from "./Control";
 
 function App() {
-    const [num, setNum] = useState(0);
-
-    const onBtnIncreaseClicked = () => setNum(num + 1);
-    const onBtnDecreaseClicked = () => setNum(num - 1);
-    
-
+ 
+   
   return (
     <>
-      NUM: {num}
-      <br />
-      <button onClick={onBtnIncreaseClicked}>+1</button>
-      <br />
-      <button onClick={onBtnDecreaseClicked}>-1</button>
-      <br />
-      <button onClick={() => setNum(num + 10)}>+10</button>
-      <br />
-      <button onClick={() => setNum(num - 10)}>-10</button>
-      <br />
+      <Control />
     </>
     
   );
