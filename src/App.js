7a124ad9b02d112f116070df2d77695e25d8e6d7
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-04-23 01:55:19 +0900

    테일윈드 적용 반응형 상품리스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-// import SetTimeOut from "./SetTimeOut";
-// import Control from "./Control";
-// import Counter from "./Counter";
-// import Popup from "./Popup";
-import ProductListItem from "./ProductListItem"
-
+import ProdList from "./ProdList";
 
 function App() {
 
   return (
     <>
-      {/* <Control /> */}
-      {/* <SetTimeOut /> */}
-      {/* <Counter/> */}
-      {/* <Popup/> */}
-      <div style={{display: "flex", gap: "10px"}}>
-        {<ProductListItem imgNo={1} productName="MAC OS" productPriceFormatted={"3,000,000원"} />}
-        {<ProductListItem imgNo={2} productName="MAC BOOK" productPriceFormatted={"4,100,000원"}/>}
-        {<ProductListItem imgNo={201} productName="MAC BOOK PRO" productPriceFormatted={"4,600,000원"}/>}
-      </div>
+     <ProdList className="container mx-auto" />
     </>
     
   );
