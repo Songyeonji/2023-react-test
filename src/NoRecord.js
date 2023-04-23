f8282bec7cde5b32edcb09ede55e83b8797ed37a
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-04-23 20:22:26 +0900

    useState와 useRef의 차이

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export default function NoRecord() {
-  const [no, setNo] = useState("");
   const [recordedNos, setRecordedNos] = useState([]);
+  const noInputRef = useRef(null);
 
-  const saveNo = () => {
-    if (no === "") {
-      alert("숫자를 입력해주세요.");
+  const saveNo = (form) => {
+    form.no.value = form.no.value.trim();
+
+    if (form.no.value.length === 0) {
+      alert("숫자를 입력해주세요.");;
       return;
     }
 
-    setRecordedNos([...recordedNos, no]);
-    setNo("");
+    setRecordedNos([...recordedNos, form.no.value]);
+    form.no.value = "";
+    noInputRef.current.focus();
   };
 
   // const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
@@ -22,14 +25,10 @@ export default function NoRecord() {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          saveNo();
+          saveNo(e.target);
         }}
       >
-        <input
-          type="number"
-          value={no}
-          onChange={(e) => setNo(e.target.valueAsNumber)}
-        />
+        <input type="number" name="no" ref={noInputRef} />
         <button type="submit">기록</button>
       </form>
       {/* 버전1 */}
