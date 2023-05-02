52e0034da6af84aa1b0168402aac78c9226e856b
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-05-02 14:59:00 +0900

    음식주문 리스트 - 메인/옵션 쓸데없는 리랜더링 줄이기

--- src/Order.js
+++ src/Order.js
@@ -1,7 +1,51 @@
-import React, { useState }  from "react";
+import React, { useState, useMemo }  from "react";
+import { useCallback } from "react";
 
+function OrderMainFood({mianFoodCount, setMainFoodCount}) {
+  return(
+    <>
+      <h2>메인 (수량 : {mianFoodCount})</h2>
+      <div>
+        <button onClick={() => setMainFoodCount(mianFoodCount + 1)}>증가</button>
+        <button onClick={() => setMainFoodCount(mianFoodCount == 1 ? 1 : mianFoodCount - 1)}>
+          감소
+        </button>
+      </div>
+    </>
+  )
+}
+
+const MemoizedOrderMainFood  = React.memo(OrderMainFood);
+
+function OrderOptions({
+  selectedCount, 
+  options, 
+  toggleAllChecked, 
+  btnAllChecked, 
+  optionCheckeds,
+  toggleOptionCheck,
+}) {
+  return(
+    <>
+      <h2>옵션 ({selectedCount} / {options.length})</h2>
+      <span onClick={toggleAllChecked} style={{paddingLeft:30, userSelect:'none'}}>{btnAllChecked ? '[v]' : '[ ]'} 전체선택</span>
+      <ul>
+        {options.map((option, index) => (
+          <li style={{userSelect:'none', cursor:'pointer'}} key={option} onClick={() => toggleOptionCheck(index)}>
+            {optionCheckeds[index] ? "[v]" : "[ ]"}
+            {option}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+const MemoizedOrderOptions  = React.memo(OrderOptions);
 
 export default function Order() {
+  const [mianFoodCount, setMainFoodCount] = useState(1);
+
   const options = [
     "사이다",
     "콜라",
@@ -15,15 +59,17 @@ export default function Order() {
   // [false,false,false,false,false,false)
   const [optionCheckeds, setOptionCheckeds] = useState(new Array(options.length).fill(false));
   
-  const toggleOptionCheck = (index) => {
+  const toggleOptionCheck = useCallback(
+    (index) => {
     const newOptionCheckeds = optionCheckeds.map((el, _index) => _index == index ? !el : el);
     setOptionCheckeds(newOptionCheckeds);
-  }
+  }, [optionCheckeds]
+);
+
+  const btnAllChecked = useMemo(() => optionCheckeds.every((el) => el), [optionCheckeds]);
+  const selectedCount = useMemo(() => optionCheckeds.filter(el => el).length, [optionCheckeds]);
   
-  const btnAllChecked = optionCheckeds.every((el) => el);
-  const selectedCount = optionCheckeds.filter(el => el).length;
-  
-  const toggleAllChecked = () => {
+  const toggleAllChecked = useCallback(() => {
     if ( btnAllChecked ) {
       // 전부 체크해제 해야함
       const newOptionCheckeds = optionCheckeds.map((el) => false);
@@ -33,23 +79,33 @@ export default function Order() {
       // 전부 체크 해야함
       const newOptionCheckeds = optionCheckeds.map((el) => true);
       setOptionCheckeds(newOptionCheckeds);
-    }
-  }
+    } 
+}, [optionCheckeds]);
 
   return (
     <>
       <h1>음식주문</h1>
-      
-      <h2>옵션 ({selectedCount} / {options.length})</h2>
-      <span onClick={toggleAllChecked} style={{paddingLeft:30, userSelect:'none'}}>{btnAllChecked ? '[x]' : '[ ]'} 전체선택</span>
-      <ul>
-        {options.map((option, index) => (
-          <li style={{userSelect:'none', cursor:'pointer'}} key={option} onClick={() => toggleOptionCheck(index)}>
-            {optionCheckeds[index] ? "[x]" : "[ ]"}
-            {option}
-          </li>
-        ))}
-      </ul>
+
+      <MemoizedOrderMainFood 
+        setMainFoodCount={setMainFoodCount} 
+        mianFoodCount={mianFoodCount}
+      />
+      <MemoizedOrderOptions 
+        selectedCount ={selectedCount} 
+        options={options}
+        toggleAllChecked={toggleAllChecked} 
+        btnAllChecked ={btnAllChecked}
+        optionCheckeds={optionCheckeds}
+        toggleOptionCheck={toggleOptionCheck}
+      />
+      <MemoizedOrderOptions
+        selectedCount ={selectedCount} 
+        options={options}
+        toggleAllChecked={toggleAllChecked} 
+        btnAllChecked ={btnAllChecked}
+        optionCheckeds={optionCheckeds}
+        toggleOptionCheck={toggleOptionCheck}
+      />
     </>
   );
 }
