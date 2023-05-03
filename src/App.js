7012f3fa724cc76aafa69894441f5a0a906de539
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-05-03 15:23:46 +0900

    셀렉트 박스 다루기

--- src/App.js
+++ src/App.js
@@ -14,7 +14,20 @@ function App() {
     setSelecteds(newSelecteds);
   };
   const selectedsFruits = selecteds.map((el, index) => el ? fruits[index] : el).filter((el) => el);
-  
+
+  const ageBands = [
+    "영유아/아동",
+    "10대",
+    "20대",
+    "30대",
+    "40대",
+    "50대",
+    "60대",
+    "그외"
+  ];
+
+  const [selectedAgeBand, setSelectedAgeBand] = useState(ageBands[3]);
+
     return (
     <>
       {/* <Order /> */}
@@ -55,6 +68,13 @@ function App() {
         여성
       </label>
       <div> 현재 값 : {selectedGender} </div>
+      <hr />
+      <select onChange={(e) => setSelectedAgeBand(e.target.value)}>
+        <option selected disabled>- 나이대역 -</option>
+        {ageBands.map(ageBand => <option selected={ageBand == selectedAgeBand} value={ageBand}>{ageBand}</option>)}
+      </select>
+      
+      <div>현재 : {selectedAgeBand}</div>
     </>
   );
 }
